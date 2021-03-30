092bfc8bbd50e771736047c4964a1ec3871c87c1
Hemalatha Marikumar <hemalatha.marikumar@syncfusion.com>
2021-03-30 10:37:07 +0530

    Changes are done

---
+++ ChipCustomization/ChipCustomization/Model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChipCustomization
+{
+    public class Language
+    {
+        public Xamarin.Forms.Command CloseButtonTapped { get; set; }
+
+        public string Name { get; set; }
+        public Language()
+        {
+            CloseButtonTapped = new Xamarin.Forms.Command(TapCloseButton);
+        }
+
+        private void TapCloseButton(object e)
+        {
+
+            ViewModel viewModel = e as ViewModel;
+            viewModel.Languages.Remove(this);
+        }
+
+    }
+}
---
+++ ChipCustomization/ChipCustomization/ViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace ChipCustomization
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+
+        private ObservableCollection<Language> languages;
+        public ObservableCollection<Language> Languages
+        {
+            get
+            {
+                return languages;
+            }
+            set
+            {
+                languages = value;
+                OnPropertyChanged("Languages");
+            }
+        }
+
+        public ViewModel()
+        {
+            Languages = new ObservableCollection<Language>();
+            Languages.Add(new Language() { Name = "C#" });
+            Languages.Add(new Language() { Name = "HTML" });
+            Languages.Add(new Language() { Name = "Java" });
+            Languages.Add(new Language() { Name = "JS" });
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
